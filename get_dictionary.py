474dfc3951a3a0b11ceb0fe7c240247e730bd228
jando311 <jandopriv@gmail.com>
2024-02-08 17:36:08 +1100

    sql'ing

--- get_dictionary.py
+++ get_dictionary.py
@@ -17,20 +17,12 @@ with open (csv_file, 'r', newline = '', encoding = 'utf-8') as file:
     next(csv_reader)
     for row in csv_reader:
         cursor.execute('INSERT INTO my_table VALUES (?,?)', row)
-    
-cursor.execute ('SELECT * FROM my_table ORDER BY RANDOM () LIMIT 1')
 
-random_row = cursor.fetchone ()
+conn.commit()
+conn.close() 
 
-conn.close()
 
-if random_row:
-    random_word, random_definition = random_row
 
-word = random_word
-unknown_word = list (len (word)*'*')
-
-print (unknown_word)
 
 
 
