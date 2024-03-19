c42eb912899588edccabf4627b4aaea4c8d690e8
jando311 <jandopriv@gmail.com>
2024-03-19 17:14:49 +1100

    cleaninnggg

--- main.py
+++ main.py
@@ -3,24 +3,29 @@ from datetime import timedelta
 import pandas as pd 
 import sqlite3
 import csv 
+import random 
 
 app = Flask (__name__)
 app.secret_key = 'whipe'
 
-conn = sqlite3.connect ('small_dictionary.db')
-cursor = conn.cursor ()
+def get_word ():
+    conn = sqlite3.connect ('small_dictionary.db')
+    cursor = conn.cursor ()
+    cursor.execute ('SELECT * FROM my_table ORDER BY RANDOM() LIMIT 1')
+    random_row = cursor.fetchone()
+    conn.close 
+    return random_row
 
-cursor.execute ('SELECT * FROM my_table ORDER BY RANDOM() LIMIT 1')
+random_word = get_word()
 
-random_row = cursor.fetchone()
+def get_hint (new_definition):
+    print ('Your hint is:', new_definition)
 
-conn.close 
+def get_hint_2 (unknown_word):
+     indexes_word = [i for i, value in enumerate (unknown_word) if value == '_']
+     random_index = random.choice (indexes_word)
+     
 
-if random_row:
-    random_word, random_definition = random_row 
-
-print (random_word)
-print (random_definition)
 
 @app.route('/')
 def home ():
@@ -53,29 +58,26 @@ def logout ():
     session.pop('user', None)
     return redirect (url_for ('login'))
 
-
 @app.route ('/game', methods = ['POST', 'GET'])
 def game ():
 
-    def get_index (variable, unknown_word):
+    if 'random_word' not in session:
+        session ['random_word'] = get_word()[0]
+        session ['random_definition'] = get_word()[1]
+        ## MAYBE CREATE NEW DEFINITION IN HERE *** MIGHT NEED DISECTING BEFORE WE CAN USE IT 
+        print (session ['random_word'])
+     
+    def get_index (variable, unknown_word, random_word):
         positions = [i for i, letter in enumerate (random_word) if letter == variable]
         for position in positions:
             unknown_word[position] = variable
-        print (unknown_word)
         return unknown_word
-         
 
-    def hangman_game_func (guess, word):
-        print (word)
-        print (guess)
+    def hangman_game_func (guess, word, random_definition):
         unknown_word = session ['unknown_word']
         tries = session ['tries']
-        guessed_letters = session ['guessed_letters']
-
-
-        print (tries)
-        print (unknown_word) 
-            
+        guessed_letters = session ['guessed_letters']    
+        
         if guess in guessed_letters and len(guess) == 1:
                     flash ("You already guessed that, try again!") 
                     return render_template ('game.html', guessed_letters = session ['guessed_letters'], tries=session ['tries'], unknown_word = session ['unknown_word'], result = session ['result'])
@@ -83,14 +85,13 @@ def game ():
 
         if guess in word and len(guess)==1 and guess.isalpha:
                     guessed_letters.append(guess) 
-                    session ['unknown_word'] = get_index (guess, unknown_word)
+                    session ['unknown_word'] = get_index (guess, unknown_word, session['random_word'])
                     if unknown_word == list (word):
                          flash ('You won, press the "New Game" Button if you want to play again!')
                     elif tries == 0:
                          flash ('You loose, press the "New Game" Button if you want to play again!')
                     else:
                         flash ("Congratulations, you found a Letter!")
-                    print (unknown_word)
                     return render_template ('game.html', guessed_letters = session ['guessed_letters'], tries=session ['tries'], unknown_word = session ['unknown_word'], result = session ['result'])
 
                      
@@ -113,10 +114,10 @@ def game ():
                     tries -= 1
                     print ("No, not the word!")
                     return render_template ('game.html', guessed_letters = session ['guessed_letters'], tries=session ['tries'], unknown_word = session ['unknown_word'], result = session ['result'])
-        
-    
+
+
     if 'unknown_word' not in session:
-        session ['unknown_word'] = list ( len (random_word)* '_')
+        session ['unknown_word'] = list ( len (session ['random_word'])* '_')
 
     if 'guessed_letters' not in session:
         session ['guessed_letters'] = []   
@@ -130,44 +131,46 @@ def game ():
     if 'hint_2_used' not in session:
         session ['hint_used'] = 'hint_2_used'
 
-    if 'variable' not in session:
-        session ['variable'] = 'variable'
+    if 'value' not in session:
+        session ['value'] = 'value'
 
     if 'result' not in session:
         session ['result'] = 'result'
 
+    print (session ['random_word'])
+
     if request.method =='POST':
         guess = request.form ['guess']
-        hangman_game_func (guess, random_word)
+        hangman_game_func (guess,  session ['random_word'], session ['random_definition'])
         return render_template ('game.html', guessed_letters = session ['guessed_letters'], tries=session ['tries'], unknown_word = session ['unknown_word'], result = session ['result'])
-  
-
+      
     return render_template ('game.html', unknown_word = session ['unknown_word'])
 
-
+@app.route ('/hints', methods = ['POST', 'GET'])
+def get_hint ():
+     if request.method == 'POST':
+        if session ['value'] == 2 and not session ['hint_used']:
+            get_hint ()
+            return redirect (url_for ('game'))
+        
+        if session ['value'] == 3 and not session ['hint_2_used']:
+            get_hint_2 ()
+            return redirect (url_for ('game'))
+               
 @app.route ('/endgame', methods = ['POST', 'GET'])
 def endgame ():
-    if request.method == 'POST':
-        conn = sqlite3.connect ('small_dictionary.db')
-        cursor = conn.cursor ()
-
-        cursor.execute ('SELECT * FROM my_table ORDER BY RANDOM() LIMIT 1')
-
-        random_row = cursor.fetchone()
-
-        conn.close 
-
-        if random_row:
-            random_word, random_definition = random_row 
-
-        print (random_word)
-        print (random_definition)
-        
-        session ['unknown_word'] = list (len (random_word)*'_')
+    if request.method == 'POST': 
+        random_row = get_word()
+        session ['unknown_word'] = list ( len (random_row[0])* '_')
         session ['guessed_letters'] = []   
         session ['tries'] = 6 
-        session ['variable'] = 'variable'
+        session ['variable'] = 'variable' 
         session ['result'] = 'result'
+        session ['random_word'] = random_row[0] 
+        session ['random_definition'] = random_row[1]
+        session ['hint_used'] = False
+        session ['hint_2_used'] = False
+        session ['value'] = 0 
         return redirect (url_for ('game'))
      
 if __name__ == '__main__':
